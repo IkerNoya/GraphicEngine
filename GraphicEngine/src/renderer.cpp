d44ff3126699cb1a6c99b3b7a2e66e1bd65bcf14
Iker <ikernoya4@gmail.com>
2020-08-28 12:30:07 -0300

    Created renderer class

---
+++ GraphicEngine/src/renderer.cpp
@@ -0,0 +1,15 @@
+#include "renderer.h"
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+Renderer::Renderer() {
+
+}
+Renderer::~Renderer() {
+
+}
+void Renderer::DrawTriangle() {
+
+}

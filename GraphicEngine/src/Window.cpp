a23b19ea7a1a3feefb3455f3ea759df2f18c2316
Iker <ikernoya4@gmail.com>
2020-08-28 12:33:37 -0300

    Deleted unused includes

--- GraphicEngine/src/Window.cpp
+++ GraphicEngine/src/Window.cpp
@@ -1,6 +1,5 @@
 #include "window.h"
 #include <stdlib.h>
-#include <string>
 #include <iostream>
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
--- GraphicEngine/src/gameBase.cpp
+++ GraphicEngine/src/gameBase.cpp
@@ -1,6 +1,5 @@
 #include "gameBase.h"
 #include <stdlib.h>
-#include <string>
 #include <iostream>
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"

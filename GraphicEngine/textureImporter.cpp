05855a4ecf9327518bedb9886ed99c8f63f740f2
Iker <ikernoya4@gmail.com>
2020-10-22 23:47:56 -0300

    Completed constructors, setters and getters from texture importer

--- GraphicEngine/textureImporter.cpp
+++ GraphicEngine/textureImporter.cpp
@@ -1,1 +1,51 @@
 #include "textureImporter.h"
+#include "stb_image.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+TextureImporter::TextureImporter() {
+	_height = 0;
+	_width = 0;
+	_nrChannels = 0;
+}
+
+TextureImporter::TextureImporter(int height, int width, int nrChannels) {
+	_height = height;
+	_width = width;
+	_nrChannels = nrChannels;
+}
+
+TextureImporter::~TextureImporter() {
+
+}
+
+void TextureImporter::setHeight(int height) {
+	_height = height;
+}
+
+int TextureImporter::getHeight() {
+	return _height;
+}
+
+void TextureImporter::setWidth(int width) {
+	_width = width;
+}
+
+int TextureImporter::getWidth() {
+	return _width;
+}
+
+void TextureImporter::setNrChanels(int nrChannels) {
+	_nrChannels = nrChannels;
+}
+
+int TextureImporter::getNrChannels() {
+	return _nrChannels;
+}
+
+void TextureImporter::loadImage() {
+	
+}
+
+void TextureImporter::generateTexture() {
+
+}
--- GraphicEngine/textureImporter.h
+++ GraphicEngine/textureImporter.h
@@ -1,9 +1,22 @@
 #ifndef TEXTURE_IMPORTER
 #define TEXTURE_IMPORTER
+
 class TextureImporter {
-
+	int _height;
+	int _width;
+	int _nrChannels;
 public:
-
+	TextureImporter();
+	TextureImporter(int height, int width, int nrChannels);
+	~TextureImporter();
+	void setHeight(int height);
+	int getHeight();
+	void setWidth(int width);
+	int getWidth();
+	void setNrChanels(int nrChannels);
+	int getNrChannels();
+	void loadImage();
+	void generateTexture();
 };
 #endif // !TEXTURE_IMPORTER
 
